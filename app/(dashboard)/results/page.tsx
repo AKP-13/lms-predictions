990ab061a141d2f750a88d87bb4f57751b7ff760
AKP-13 <alexpeirson@hotmail.com>
2024-10-29 16:59:42 +0000

    Add Results route and page

---
+++ app/(dashboard)/results/page.tsx
@@ -0,0 +1,22 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+
+const PredictionsPage = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Results</CardTitle>
+        <CardDescription>View your previous results</CardDescription>
+      </CardHeader>
+
+      <CardContent></CardContent>
+    </Card>
+  );
+};
+
+export default PredictionsPage;
