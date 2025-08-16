2401ce6c9dfff3935a930c0d7ad34186d5ec081e
AKP-13 <alexpeirson@hotmail.com>
2025-08-16 10:15:38 +0100

    Add GitHub and Resend Magic Link sign in options

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,34 @@
 import NextAuth from 'next-auth';
+import NeonAdapter from '@auth/neon-adapter';
+import { Pool } from '@neondatabase/serverless';
 import GitHub from 'next-auth/providers/github';
+import Resend from 'next-auth/providers/resend';
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub]
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
+  // Create a `Pool` inside the request handler.
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  return {
+    adapter: NeonAdapter(pool),
+    providers: [
+      GitHub,
+      Resend({
+        // If your environment variable is named differently than default
+        apiKey: process.env.AUTH_RESEND_KEY,
+        from: 'noreply@lmsiq.co.uk'
+      })
+    ],
+    pages: {
+      signIn: '/login', // where your login form lives
+      //   signOut: '/', // after logging out
+      //   error: '/login', // error page
+      //   verifyRequest: '/verify', // (optional) "Check your email" page
+      newUser: '/' // (optional) first login
+    },
+    callbacks: {
+      async redirect({ url, baseUrl }) {
+        // Always redirect to homepage after sign-in
+        return baseUrl;
+      }
+    }
+  };
 });
