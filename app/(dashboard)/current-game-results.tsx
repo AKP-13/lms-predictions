31230c97dd9731f0fb6bcff11276dd7a2d053dc5
AKP-13 <alexpeirson@hotmail.com>
2025-09-27 10:38:02 +0100

    Refactor CurrentGameResults component to use hooks for data fetching and add error handling for game data retrieval

--- app/(dashboard)/current-game-results.tsx
+++ app/(dashboard)/current-game-results.tsx
@@ -15,20 +15,31 @@ import {
   TeamName,
   TeamScore
 } from '@/components/ui/table';
-import { auth } from '@/lib/auth';
-import { fetchCurrentGameData } from '@/lib/data';
+import useCurrentGameData from 'app/hooks/useCurrentGameData';
+import { useSession } from 'next-auth/react';
+import { Fragment } from 'react';
 
-const CurrentGameResults = async () => {
-  const session = await auth();
-  const currentGameResults = await fetchCurrentGameData({
-    userId: session?.user?.id
+const CurrentGameResults = ({
+  leagueInfo,
+  refreshTrigger
+}: {
+  leagueInfo: null | string;
+  refreshTrigger: number;
+}) => {
+  const { data: session } = useSession();
+  const { currentGameResults, isLoading } = useCurrentGameData({
+    refreshTrigger
   });
 
   return (
     <Card className="rounded-xl bg-white p-2 my-8 shadow-sm overflow-auto">
       <CardHeader>
-        <CardTitle>Current Game</CardTitle>
-        <CardDescription>Your results from this game</CardDescription>
+        <CardTitle>
+          Current Game{leagueInfo ? ` - ${leagueInfo}` : ''}
+        </CardTitle>
+        <CardDescription>
+          {`Your results from this game ${currentGameResults.length === 0 ? 'will be displayed here.' : ''}`}
+        </CardDescription>
       </CardHeader>
 
       <CardContent>
@@ -41,6 +52,10 @@ const CurrentGameResults = async () => {
               Sign in to get started
             </a>
           </div>
+        ) : leagueInfo === null ? (
+          'Join a league to get started.'
+        ) : currentGameResults.length === 0 ? (
+          'Submit a prediction to begin seeing results here.'
         ) : (
           <Table>
             <TableHeader>
@@ -55,9 +70,8 @@ const CurrentGameResults = async () => {
             <TableBody>
               <TableRow>
                 {currentGameResults.map((prediction, gameIdx) => (
-                  <>
+                  <Fragment key={`gw-fragment-${gameIdx}`}>
                     <TableCell
-                      key={`gw-cell-${gameIdx}`}
                       className={`table-cell ${prediction.correct === true ? 'bg-green-200' : prediction.correct === false ? 'bg-red-200' : 'bg-gray-200'}`}
                     >
                       <div
@@ -74,7 +88,16 @@ const CurrentGameResults = async () => {
                           <TeamName location="Away" prediction={prediction} />
                         </div>
                         {prediction.correct === null ? (
-                          'Pending...'
+                          <a
+                            href={`mailto:alexlmsapp@icloud.com?subject=Last%20Player%20Standing%20Prediction%20&body=I%20would%20like%20to%20edit%20my%20prediction%20to...`}
+                            style={{
+                              color: 'blue',
+                              textDecoration: 'underline',
+                              fontWeight: 600
+                            }}
+                          >
+                            Edit
+                          </a>
                         ) : (
                           <div>
                             <TeamScore
@@ -100,18 +123,17 @@ const CurrentGameResults = async () => {
                             display: 'flex',
                             flexDirection: 'column',
                             textAlign: 'center',
-                            minWidth: '4rem',
-                            maxWidth: '9rem'
+                            width: '100%'
                           }}
                         >
-                          <span>You're out of this game!</span>
                           <span>
-                            You will get an email when the new game starts.
+                            You are eliminated and will get an email when the
+                            new game starts.
                           </span>
                         </div>
                       </TableCell>
                     )}
-                  </>
+                  </Fragment>
                 ))}
               </TableRow>
             </TableBody>
---
+++ app/api/currentGameData/route.ts
@@ -0,0 +1,21 @@
+import { auth } from '@/lib/auth';
+import { fetchCurrentGameData, fetchResultsData } from '@/lib/data';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const session = await auth();
+
+    const data = await fetchCurrentGameData({
+      userId: session?.user?.id ?? undefined
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.log('error fetching current game data', error);
+    return NextResponse.json(
+      { error: 'Failed to fetch results.' },
+      { status: 500 }
+    );
+  }
+}
