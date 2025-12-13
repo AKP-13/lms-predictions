84a029b440da35b46d1751b4e25fbff37a766fd2
AKP-13 <alexpeirson@hotmail.com>
2025-12-13 17:43:03 +0000

    Refactor useResults hook to conditionally fetch data based on session availability

--- app/hooks/useResults.ts
+++ app/hooks/useResults.ts
@@ -1,21 +1,28 @@
 import { useEffect, useState } from 'react';
 import { Results } from '@/lib/definitions';
+import { useSession } from 'next-auth/react';
 
 const useResults = ({ refreshTrigger }: { refreshTrigger: number }) => {
   const [results, setResults] = useState<Record<number, Results[]>>({});
   const [isLoadingResults, setIsLoadingResults] = useState(true);
 
+  const { data: session } = useSession();
+
   useEffect(() => {
-    async function fetchResults() {
+    const fetchData = async () => {
       setIsLoadingResults(true);
       const res = await fetch('/api/results');
       const results = await res.json();
       setResults(results);
       setIsLoadingResults(false);
-    }
+    };
 
-    fetchResults();
-  }, [refreshTrigger]);
+    if (session) {
+      fetchData();
+    } else {
+      setIsLoadingResults(false);
+    }
+  }, [refreshTrigger, session]);
 
   return { results, isLoadingResults };
 };
